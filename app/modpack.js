fa79a99554c44eb8894752da4ab6f1c1b197e46f
Ralf Lauseker <midnights.straychild@gmail.com>
2016-01-03 17:46:58 +0100

    added zip compression

--- app/modpack.js
+++ app/modpack.js
@@ -5,6 +5,7 @@
  */
 var github = require("./github"),
     fs = require("fs-extra"),
+    archiver = require("archiver"),
     config = require("./config.js").config,
     logger = require("./logger.js").getLogger(),
     email = require("./email.js"),
@@ -63,42 +64,48 @@ var github = require("./github"),
      * @param branchOrTag
      */
     createPack = function (path, branchOrTag) {
-        var pack = config.packName + "-" + branchOrTag;
+        var pack = config.packName + "-" + branchOrTag,
+            packWorkingPath = config.paths.packages + pack,
+            packFilePath = config.paths.packages + pack + ".zip";
 
         logger.info("Packing everthing up.");
 
-        // Removing old file if still there
+        // Remove old working folder pack
         try {
             if (fs.statSync(config.paths.packages + pack).isDirectory()) {
-                deleteFolderRecursive(config.paths.packages + pack + "/");
-                logger.info("Removed existing pack '" + config.paths.packages + pack + "'");
+                deleteFolderRecursiveSync(config.paths.packages + pack);
+                logger.info("Removed working folder pack '" + config.paths.packages + pack + "'");
             }
         } catch (e) {
-            // do nothing
+            logger.error("nothing deleted. " + e);
         }
 
         // Created folder for pack
-        fs.mkdir(config.paths.packages + pack);
+        if (!fs.existsSync(config.paths.packages + pack)) {
+            fs.mkdirSync(config.paths.packages + pack);
+        }
         logger.info("Created folder for pack '" + config.paths.packages + pack + "'");
 
+        // Copy Files to release directory
         logger.info("Copy Files to release directory...");
-
+        // Copy ressourcepack to release directory
         if (fs.statSync(config.paths.resourcepacks).isDirectory()) {
-            fs.copy(
+            fs.copySync(
                 config.paths.resourcepacks,
                 config.paths.packages + pack + "/" + config.paths.resourcepacks,
-                { preserveTimestamps: true }
+                {preserveTimestamps: true}
             );
             logger.info("Added resource packs from '" + config.paths.resourcepacks + "'.");
         }
 
+        // Copy repo files to release directory
         ["bin", "mods", "config"].forEach(function (folder) {
             try {
                 if (fs.statSync(path + folder).isDirectory()) {
-                    fs.copy(
+                    fs.copySync(
                         path + folder,
                         config.paths.packages + pack + "/" + folder,
-                        { preserveTimestamps: true }
+                        {preserveTimestamps: true}
                     );
                     logger.info("Added '" + folder + "' from '" + path + "'.");
                 }
@@ -112,6 +119,26 @@ var github = require("./github"),
         });
 
         logger.info("Put everything to '" + config.paths.packages + pack + "'.");
+
+        // Remove old pack file
+        try {
+            if (fs.statSync(packFilePath).isFile()) {
+                fs.unlink(packFilePath);
+                logger.info("Removed old pack file '" + packFilePath + "'");
+            }
+        } catch (e) {
+            // do nothing
+        }
+
+        // Zip working directory
+        logger.info("Starting to compress working directory '" + packFilePath + "'...");
+
+        try {
+            compressWorkingDirectory(packWorkingPath, packFilePath);
+        } catch (e) {
+            logger.error(e);
+        }
+
 
         // Send Mail
         //try {
@@ -127,21 +154,49 @@ var github = require("./github"),
      *
      * @param path
      */
-    deleteFolderRecursive = function(path) {
+    deleteFolderRecursiveSync = function (path) {
         var files = [];
-        if( fs.existsSync(path) ) {
+        if (fs.existsSync(path)) {
             files = fs.readdirSync(path);
-            files.forEach(function(file,index){
+            files.forEach(function (file, index) {
                 var curPath = path + "/" + file;
-                if(fs.lstatSync(curPath).isDirectory()) { // recurse
-                    deleteFolderRecursive(curPath);
+                if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                    deleteFolderRecursiveSync(curPath);
                 } else { // delete file
                     fs.unlinkSync(curPath);
                 }
             });
             fs.rmdirSync(path);
         }
-    };;
+    },
+
+    /**
+     *
+     * @param packWorkingPath
+     * @param packFilePath
+     */
+    compressWorkingDirectory = function (packWorkingPath, packFilePath) {
+        var output = fs.createWriteStream(packFilePath),
+            archive = archiver('zip');
+
+        output.on('close', function () {
+            logger.info(archive.pointer() + ' total bytes');
+            logger.info('archiver has been finalized and the output file descriptor has closed.');
+        });
+
+        archive.on('error', function (err) {
+            throw err;
+        });
+
+        archive.pipe(output);
+
+        archive.bulk([
+            {expand: true, cwd: packWorkingPath, src: ['**/*']}
+        ]);
+
+        archive.finalize();
+    };
+
 
 exports.createModPackFromBranch = createModPackFromBranch;
 exports.createModPackFromTag = createModPackFromTag;
